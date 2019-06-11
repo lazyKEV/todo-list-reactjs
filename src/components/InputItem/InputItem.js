de498645554d959659f4d97dfbf14e12f79c3208
lazyKEV <viveksinha.301@gmail.com>
2019-06-11 22:05:17 +0530

    completed input handling

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import './InputItem.css'
 
 
 const InputItem = (props) => {
     return (
-        <div>
-            <input type="text" />
-            <input type="text" />
-            <button type="submit">Submit</button>
-        </div>
+        <form onSubmit={props.submitForm} className="form-group">
+            <div className="form-control">
+                <input
+                    type="text"
+                    placeholder="Title"
+                    value={props.item.title}
+                    onChange={
+                        (event) => props.changeInput('title', event)
+                    }
+                />
+            </div>
+            <div className="form-control">
+                <textarea
+                    type="text"
+                    placeholder="Description..."
+                    value={props.item.description}
+                    onChange={
+                        (event) => props.changeInput('description', event)
+                    }
+                />
+            </div>
+            <div className="form-control">
+                <input type="submit" value="Submit" />
+            </div>
+        </form>
     );
 }
 
--- src/container/App.js
+++ src/container/App.js
@@ -1,35 +1,83 @@
 import React, { Component } from 'react';
 import uuid4 from 'uuid/v4'
+import moment from 'moment'
 import './App.css';
 import TodoHeader from '../components/TodoHeader/TodoHeader';
 import TodoList from '../components/TodoList/TodoList';
 import InputItem from '../components/InputItem/InputItem';
 
 class App extends Component {
-  state = {
-    items: [
-      {
-        id: uuid4(),
-        title: "Hello",
-        description: "Testing to-do list",
-        date: "2019-06-11"
-      },
-      {
-        id: uuid4(),
-        title: "Hi",
-        description: "Testing to-do list 2",
-        date: "2019-06-11"
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [],
+      itemForm: {
+        title: '',
+        description: ''
       }
-    ]
+    }
+    this.formSubmitHandler.bind(this);
+    this.formInputHandler.bind(this);
+    this.removeAllHandler.bind(this);
+  }
+
+  formSubmitHandler = (event) => {
+    event.preventDefault()
+
+    // title should not be empty
+    if (this.state.itemForm.title === '') return;
+    let itemForm = this.state.itemForm
+    itemForm.title = itemForm.title.trim()
+    itemForm.description = itemForm.description.trim()
+
+    const newItem = {
+      id: uuid4(),
+      ...itemForm,
+      date: moment.utc().unix()
+    }
+
+    console.log(newItem)
+    this.setState(prevState => ({
+      items: prevState.items.concat(newItem),
+      itemForm: {
+        title: '',
+        description: ''
+      }
+    }));
+  }
+
+  formInputHandler = (key, event) => {
+    this.setState({
+      itemForm: {
+        ...this.state.itemForm,
+        [key]: event.target.value
+      }
+    });
+  }
+
+  removeAllHandler = (event) => {
+    event.preventDefault();
+    this.setState({
+      items: []
+    })
+  }
+
+  removeItem = (key, event) => {
+    event.preventDefault();
+    this.setState(prevState => ({
+      items: prevState.items.filter(item => {
+        return item.id !== key;
+      })
+    }));
   }
 
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         My To-do Lists
-        <TodoHeader />
+        <TodoHeader removeAll={this.removeAllHandler} />
         <TodoList items={this.state.items} />
-        <InputItem />
+        <InputItem submitForm={this.formSubmitHandler} item={this.state.itemForm} changeInput={this.formInputHandler} />
       </div>
     );
   }
