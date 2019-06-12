325b979902940f8ba452739ee54fc754c5ebc77a
lazyKEV <viveksinha.301@gmail.com>
2019-06-12 13:31:15 +0530

    finalizing css with remove icon

--- src/components/TodoList/TodoListItem/TodoListItem.js
+++ src/components/TodoList/TodoListItem/TodoListItem.js
@@ -4,8 +4,16 @@ import './TodoListItem.css'
 const TodoListItem = (props) => {
     return (
         <div className="TodoListItem">
-            {props.item.title} {props.item.description} {props.item.date}
-            <button onClick={(event) => props.remove(props.item.id, event)}>Remove</button>
+            <div class="note-title">
+                {props.item.title}
+            </div>
+            <div class="note-desc">
+                {props.item.description}
+            </div>
+            <div class="note-del">
+                <span>{props.item.date}</span>
+                <span><i class="fa fa-trash" aria-hidden="true" onClick={(event) => props.remove(props.item.id, event)}></i></span>
+            </div>
         </div>
     );
 }
--- src/components/TodoNav/TodoNav.js
+++ src/components/TodoNav/TodoNav.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './TodoNav.css'
 
 
 const TodoNav = (props) => {
     return (
-        <div className="nav">
+        <div className="TodoNav">
             To-Do List
             {/* <button type="submit" onClick={props.removeAll}>Remove All</button>  */}
         </div>
